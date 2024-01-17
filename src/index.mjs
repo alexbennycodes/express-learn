cac48a919b042c3e254bf85106aef592bfae29bb
Alex <alexbenny2811@gmail.com>
2024-01-17 22:40:17 +0530

    feat: route params added for users and products

--- src/index.mjs
+++ src/index.mjs
@@ -11,13 +11,27 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-  res.status(201).send({ msg: "Hello, world!" });
+  res.status(200).send("Hello, world!");
 });
 
 app.get("/users", (req, res) => {
-  res.status(201).send(USER_DATA);
+  res.status(200).send(USER_DATA);
+});
+
+app.get("/users/:id", (req, res) => {
+  const getUser = USER_DATA.find((user) => user.guid === req.params.id);
+  if (getUser) res.status(200).send(getUser);
+  else res.status(404).send({ message: "Not Found!" });
 });
 
 app.get("/products", (req, res) => {
-  res.status(201).send(PRODUCTS_DATA);
+  res.status(200).send(PRODUCTS_DATA);
+});
+
+app.get("/products/:id", (req, res) => {
+  const getProduct = PRODUCTS_DATA.find(
+    (product) => product.id === req.params.id
+  );
+  if (getProduct) res.status(200).send(getProduct);
+  else res.sendStatus(404);
 });
